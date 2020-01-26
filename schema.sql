05934575bb0e2fd9bd88e961a9ab36fe58a45283
Clayton Jones <claytonjonesLMP@gmail.com>
2020-01-25 17:36:37 -0800

    finished feature 2 server

--- schema.sql
+++ schema.sql
@@ -2,8 +2,13 @@ DROP TABLE IF EXISTS cities;
 
 CREATE TABLE cities (
   id SERIAL PRIMARY KEY,
-  name TEXT,
+  search_query TEXT,
+  formatted_query TEXT,
   latitude DECIMAL,
   longitude DECIMAL
 );
+
+INSERT INTO cities
+(search_query, formatted_query, latitude, longitude)
+VALUES ('portland', 'Portland, OR Test', 45.5051, 122.6750);
 
